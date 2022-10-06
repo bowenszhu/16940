05da003e4cb387ff6a8848eba01fe3892705ea7a
Bowen S. Zhu <bszhu@fas.harvard.edu>
2022-10-06 17:02:48 -0400

    proj1 p2b

--- src/proj1/p2a.jl
+++ src/proj1/p2a.jl
@@ -2,6 +2,7 @@ using Random, Statistics, Plots, LaTeXStrings
 
 g(x) = exp(-x^2 / 2) / sqrt(2π)
 πᵗ(x) = exp(-x^2 / 2) * (sin(6x)^2 + 3cos(x)^2 * sin(4x)^2 + 1)
+h = abs2
 C = 5sqrt(2π)
 
 n_target = 100_000
@@ -26,12 +27,12 @@ write(io, "t = $t\n")
 t = 100_000
 trial = 10_000
 n = Vector{Int}(undef, trial)
-Iⁿ⁽ᵗ⁾ = similar(n, Float64)
+Îⁿ⁽ᵗ⁾ₐᵣ = similar(n, Float64)
 y = Vector{Float64}(undef, t)
 uCgy = similar(y)
 u = similar(y)
 πy = u # reuse memory
-accept = similar(uCgy, Bool)
+accept = similar(y, Bool)
 for i in 1:trial
     randn!(y) # y ~ g
     map!(πᵗ, πy, y) # π(y)
@@ -40,14 +41,15 @@ for i in 1:trial
     @. uCgy *= u * C # u * C * g(y)
     map!(<, accept, uCgy, πy) # u * C * g(y) < π(y)
     n[i] = sum(accept)
-    Iⁿ⁽ᵗ⁾[i] = mean(abs2, y[accept])
+    Îⁿ⁽ᵗ⁾ₐᵣ[i] = mean(h, y[accept])
 end
-var_Iⁿ⁽ᵗ⁾ = var(Iⁿ⁽ᵗ⁾)
-println("var(Iⁿ⁽ᵗ⁾) = ", var_Iⁿ⁽ᵗ⁾)
-write(io, "var(Iⁿ⁽ᵗ⁾) = $var_Iⁿ⁽ᵗ⁾\n")
+var_Îⁿ⁽ᵗ⁾ₐᵣ = var(Îⁿ⁽ᵗ⁾ₐᵣ)
+println("var(Îⁿ⁽ᵗ⁾ₐᵣ) = ", var_Îⁿ⁽ᵗ⁾ₐᵣ)
+write(io, "var(Îⁿ⁽ᵗ⁾ₐᵣ) = $var_Îⁿ⁽ᵗ⁾ₐᵣ\n")
 close(io)
 
 histogram(n, legend = false, xlabel = L"n", title = L"t = %$t")
 savefig("p2a_n.svg")
-histogram(Iⁿ⁽ᵗ⁾, legend = false, xlabel = L"\hat I^{n(t)}_\mathrm{AR}", title = L"t = %$t")
+histogram(Îⁿ⁽ᵗ⁾ₐᵣ, legend = false, xlabel = L"\hat I^{n(t)}_\mathrm{AR}",
+          title = L"t = %$t")
 savefig("p2a_I.svg")
---
+++ src/proj1/p2b.jl
@@ -0,0 +1,29 @@
+using Random, Statistics, Plots, LaTeXStrings
+
+g(x) = exp(-x^2 / 2) / sqrt(2π)
+πᵗ(x) = exp(-x^2 / 2) * (sin(6x)^2 + 3cos(x)^2 * sin(4x)^2 + 1)
+w̃(x) = πᵗ(x) / g(x)
+h = abs2
+
+t = 100_000
+trial = 10_000
+Îᵗᵢₛ = Vector{Float64}(undef, trial)
+x = Vector{Float64}(undef, t)
+w̃x = similar(x)
+hxw̃x = x # reuse memory
+for i in 1:trial
+    randn!(x)
+    map!(w̃, w̃x, x) # w̃(x)
+    map!(h, hxw̃x, x) # h(x)
+    hxw̃x .*= w̃x # h(x)w̃(x)
+    Îᵗᵢₛ[i] = sum(hxw̃x) / sum(w̃x)
+end
+var_Îᵗᵢₛ = var(Îᵗᵢₛ)
+println("var(Îᵗᵢₛ) = ", var_Îᵗᵢₛ)
+
+open("p2b.txt", "w") do io
+    write(io, "var(Îᵗᵢₛ) = $var_Îᵗᵢₛ\n")
+end
+
+histogram(Îᵗᵢₛ, legend = false, xlabel = L"\hat I^t_\mathrm{IS}", title = L"t = %$t")
+savefig("p2b_I.svg")
